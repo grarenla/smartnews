a865f72bbda7ed136fcb1a52fc91d2d28286ca5f
daotung <daotungsl@gmail.com>
2019-02-21 09:58:55 +0700

    fix ve ngrok them seeder

--- database/migrations/2019_02_14_090731_create_categories_table.php
+++ database/migrations/2019_02_14_090731_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoiesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateCategoiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categoies');
+        Schema::dropIfExists('category');
     }
 }
