07ee58388eb2a2c01afc93f54b3b55190244e255
Duongninh0110 <duongninh0110@gmail.com>
2019-03-05 15:50:08 +0700

    return old cors middle ware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,11 +3,18 @@ namespace App\Http\Middleware;
 use Closure;
 class Cors
 {
-  public function handle($request, Closure $next)
-  {
-    return $next($request)
-      ->header(‘Access-Control-Allow-Origin’, ‘*’)
-      ->header(‘Access-Control-Allow-Methods’, ‘GET, POST, PUT, DELETE, OPTIONS’)
-      ->header(‘Access-Control-Allow-Headers’, ‘X-Requested-With, Content-Type, X-Token-Auth, Authorization’);
-  }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+    }
 }
