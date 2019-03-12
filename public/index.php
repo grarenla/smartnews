ecffaf71f52d7435c591479ca463291e57fae977
Duongninh0110 <duongninh0110@gmail.com>
2019-03-12 14:48:13 +0700

    cors authorisation

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header('Access-Control-Allow-Credentials: true');
 
 
